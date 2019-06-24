ad08ccf982f0d4796a50eca8a4104e7bdf855033
srikanya <srikanya659@gmail.com>
2019-06-24 10:03:34 +0530

    done

---
+++ Packages/numerical.py
@@ -0,0 +1,26 @@
+#Function to check given no is prime or not
+
+def ischeck(n):
+    flag=1
+    if n == 2:
+        return True
+    for i in range(2,n//2 + 1):
+        if n % i== 0:
+            flag= 0
+            return False
+    if flag == 1:
+        return True
+
+
+
+#Primefactors
+
+def numberprimefactors(n):
+    if ischeck(n):
+        return 1
+    count=0
+    for i in range(2,n // 2+1):
+        if ischeck(i) and n % i==0:
+            count=count+1
+    return count
+
